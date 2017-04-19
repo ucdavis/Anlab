d7817b2e7ee0a24451e7f118a00fe9217a510040
Scott Kirkland <srkirkland@gmail.com>
2017-04-19 14:54:02 -0700

    bring in lots of support files

---
+++ Anlab.Mvc/Models/ManageViewModels/AddPhoneNumberViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnlabMvc.Models.ManageViewModels
+{
+    public class AddPhoneNumberViewModel
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ Anlab.Mvc/Services/IEmailSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AnlabMvc.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
