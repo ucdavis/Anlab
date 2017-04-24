7c095b6cf57076bc08943d6f0af8a35b8acb2db6
Scott Kirkland <srkirkland@gmail.com>
2017-04-24 15:30:37 -0700

    admin controller

--- Anlab.Mvc/Controllers/AdminController.cs
+++ Anlab.Mvc/Controllers/AdminController.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnlabMvc.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnlabMvc.Controllers
 {
-    public class AdminController : Controller
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ApplicationController
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AdminController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
