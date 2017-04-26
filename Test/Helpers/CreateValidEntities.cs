33a9c7b5f9b2262fd2103155def0c9427f2d18ba
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-26 15:12:36 -0700

    Create a valid order

--- Test/Helpers/CreateValidEntities.cs
+++ Test/Helpers/CreateValidEntities.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Anlab.Core.Domain;
 
 namespace Test.Helpers
 {
     public static class CreateValidEntities
     {
+        public static Order Order(int? counter, bool populateAllFields = false)
+        {
+            var rtValue = new Order();
+            rtValue.CreatorId = string.Format("CreatorId{0}", counter);
+            rtValue.Project = string.Format("Project{0}", counter);
+
+            if (populateAllFields)
+            {
+                rtValue.Creator = new User(); //Meh? test later
+                rtValue.LabId = string.Format("LabId{0}", counter);
+                rtValue.ClientId = string.Format("ClientId{0}", counter);
+                rtValue.AdditionalEmails = string.Format("AdditionalEmails{0}", counter);
+                rtValue.JsonDetails = string.Format("JsonDetails{0}", counter);
+                rtValue.Created = DateTime.Now;
+                rtValue.Updated = DateTime.Now;
+            }
+
+
+            rtValue.Id = counter ?? 99;
+
+            return rtValue;
+        }
     }
 }
