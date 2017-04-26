cb0b7e8444fbf0cf44ce77d4bb92a97fe48a08c3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-26 15:56:58 -0700

    Not working

---
+++ Test/TestsDatabase/OrderDatabaseTests.cs
@@ -0,0 +1,29 @@
+﻿using AnlabMvc.Data;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Test.TestsDatabase
+{
+    [Trait("Category", "DatabaseTests")]
+    public class OrderDatabaseTests
+    {
+        [Fact]
+        public void test1()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
