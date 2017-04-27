d22acc82e717ca9854f7536dae83ebc6ba92ba4d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-27 09:32:48 -0700

    db tests (testing several calls to make sure db is fresh)

--- Test/TestsDatabase/OrderDatabaseTests.cs
+++ Test/TestsDatabase/OrderDatabaseTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Anlab.Core.Domain;
 using AnlabMvc.Data;
 using Microsoft.Data.Sqlite;
@@ -101,6 +102,66 @@ namespace Test.TestsDatabase
                     var xxx = context.Orders.ToList();
 
                     xxx.Count().ShouldBe(1);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        public void TestWithTheory(int value)
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var xxx = context.Orders.ToList();
+
+                    xxx.Count().ShouldBe(0);
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Users.Add(CreateValidEntities.User(1));
+                    context.SaveChanges();
+
+                    for (int i = 0; i < value; i++)
+                    {
+
+                        var order = CreateValidEntities.Order(i+1);
+                        order.Creator = context.Users.FirstOrDefault();
+                        context.Orders.Add(order);
+                    }
+
+
+                    context.SaveChanges();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var xxx = context.Orders.ToList();
+
+                    xxx.Count().ShouldBe(value);
                 }
             }
             finally
