d7c1b31db5bef7e981466f4ffd72604a794c74bc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-27 16:27:38 -0700

    working mocked identity

--- Test/TestsController/ProfileControllerTests.cs
+++ Test/TestsController/ProfileControllerTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Anlab.Core.Domain;
 using AnlabMvc.Controllers;
 using AnlabMvc.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -25,16 +26,13 @@ namespace Test.TestsController
         [Fact]
         public async Task TestWithMoqDb()
         {
-            //var context = new Mock<HttpContextBase>();
-            //var mockIdentity = new Mock<IIdentity>();
-            //context.SetupGet(x => x.User.Identity).Returns(mockIdentity.Object);
-            //mockIdentity.Setup(x => x.Name).Returns("test_name");
-
+            var user = CreateValidEntities.User(2);
+            user.Id = "1";
             // Arrange
             var data = new List<User>
             {
                 CreateValidEntities.User(1),
-                CreateValidEntities.User(2),
+                user,
                 CreateValidEntities.User(3)
             }.AsQueryable();
 
@@ -42,15 +40,24 @@ namespace Test.TestsController
             var mockContext = new Mock<ApplicationDbContext>();
             mockContext.Setup(m => m.Users).Returns(data.MockAsyncDbSet().Object);
 
-
-            var currentUser = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            var user2 = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
             {
                 new Claim(ClaimTypes.NameIdentifier, "1"),
             }));
 
-            var controller = new ProfileController(mockContext.Object)
+
+            var mockPrincipal = new Mock<IPrincipal>();
+            mockPrincipal.Setup(x => x.IsInRole(It.IsAny<string>())).Returns(true);
+
+            var mockHttpContext = new Mock<HttpContext>();
+            mockHttpContext.Setup(m => m.User).Returns(user2);
+
+
+
+            var controller = new ProfileController(mockContext.Object);
+            controller.ControllerContext = new ControllerContext
             {
-                HttpContext = { User = currentUser }
+                HttpContext = mockHttpContext.Object
             };
 
             // Act
@@ -59,7 +66,7 @@ namespace Test.TestsController
             // Assert		
             var result = Assert.IsType<ViewResult>(controllerResult);
             var model = Assert.IsType<User>(result.Model);
-            model.FirstName.ShouldBe("FirstName1");
+            model.FirstName.ShouldBe("FirstName2");
         }
 
         [Fact]
