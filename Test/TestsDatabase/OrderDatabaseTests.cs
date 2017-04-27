77f1b80fa593df1d818395ac7ba6e62c35f31d01
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-04-27 08:53:27 -0700

    Write to db? test

--- Test/TestsDatabase/OrderDatabaseTests.cs
+++ Test/TestsDatabase/OrderDatabaseTests.cs
@@ -1,6 +1,11 @@
-﻿using AnlabMvc.Data;
+﻿using System.Linq;
+using Anlab.Core.Domain;
+using AnlabMvc.Data;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Shouldly;
+using Test.Helpers;
 using Xunit;
 
 namespace Test.TestsDatabase
@@ -19,6 +24,70 @@ namespace Test.TestsDatabase
                 var options = new DbContextOptionsBuilder<ApplicationDbContext>()
                     .UseSqlite(connection)
                     .Options;
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Users.Add(CreateValidEntities.User(1));
+                    context.SaveChanges();
+
+                    var order = CreateValidEntities.Order(1);
+                    order.Creator = context.Users.FirstOrDefault();
+                    context.Orders.Add(order);
+                    context.SaveChanges();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var xxx = context.Orders.ToList();
+
+                    xxx.Count().ShouldBe(1);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [Fact]
+        public void test2()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Database.EnsureCreated();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    context.Users.Add(CreateValidEntities.User(1));
+                    context.SaveChanges();
+
+                    var order = CreateValidEntities.Order(1);
+                    order.Creator = context.Users.FirstOrDefault();
+                    context.Orders.Add(order);
+                    context.SaveChanges();
+                }
+
+                using (var context = new ApplicationDbContext(options))
+                {
+                    var xxx = context.Orders.ToList();
+
+                    xxx.Count().ShouldBe(1);
+                }
             }
             finally
             {
