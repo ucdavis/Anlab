efb528ff0b64cadb0914f324d134949b5259eef5
Scott Kirkland <srkirkland@gmail.com>
2017-05-01 15:15:30 -0700

    reset DB and replace w/ new users

---
+++ Anlab.Mvc/Controllers/SystemController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anlab.Core.Domain;
+using AnlabMvc.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnlabMvc.Controllers
+{
+    [Authorize]
+    public class SystemController : ApplicationController
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<User> _userManager;
+
+        public SystemController(ApplicationDbContext context, UserManager<User> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ResetDb()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            await DbInitializer.Initialize(_context, _userManager);
+
+            await _context.Database.EnsureCreatedAsync();
+            
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
--- Anlab.Mvc/Data/DbInitializer.cs
+++ Anlab.Mvc/Data/DbInitializer.cs
@@ -1,17 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Anlab.Core.Domain;
 using AnlabMvc.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AnlabMvc.Data
 {
     // Run on app startup for now to seed DB with initial values
     public static class DbInitializer
     {
-        public static void Initialize(ApplicationDbContext context)
+        public static async Task Initialize(ApplicationDbContext context, UserManager<User> userManager)
         {
             context.Database.EnsureCreated();
+
+            if (context.Users.Any()) return; // Do nothing if there is already user data in the system
+
+            var scottUser = new User
+            {
+                Email = "srkirkland@ucdavis.edu",
+                UserName = "srkirkland@ucdavis.edu",
+                Name = "Scott Kirkland"
+            };
+
+            var userPrincipal = new ClaimsPrincipal();
+            userPrincipal.AddIdentity(new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.NameIdentifier, "postit"),
+                new Claim(ClaimTypes.Name, "Scott Kirkland")
+            }));
+            var loginInfo = new ExternalLoginInfo(userPrincipal, "CAS", "postit", null);
+
+            await userManager.CreateAsync(scottUser);
+            await userManager.AddLoginAsync(scottUser, loginInfo);
+
+            var jasonUser = new User
+            {
+                Email = "jsylvestre@ucdavis.edu",
+                UserName = "jsylvestre@ucdavis.edu",
+                Name = "Jason Sylvestre"
+            };
+
+            var jasonUserPrincipal = new ClaimsPrincipal();
+            userPrincipal.AddIdentity(new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.NameIdentifier, "jsylvest"),
+                new Claim(ClaimTypes.Name, "Jason Sylvestre")
+            }));
+            var jasonLoginInfo = new ExternalLoginInfo(jasonUserPrincipal, "CAS", "jsylvest", null);
+
+            await userManager.CreateAsync(jasonUser);
+            await userManager.AddLoginAsync(jasonUser, jasonLoginInfo);
+
+            await context.SaveChangesAsync();
+
 
             // Seed with orders here, and maybe create users to test with
         }
