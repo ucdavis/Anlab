1a10144019a357c81a702f9cc0782152c28cdea8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-05-02 10:52:25 -0700

    Version and Improved copyright

---
+++ Anlab.Mvc/Helpers/VersionFinder.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+namespace AnlabMvc.Helpers
+{
+    public static class VersionFinder
+    {
+        private static string _version;
+
+        public static string Version
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(_version))
+                {
+                    _version = typeof(VersionFinder).GetTypeInfo()
+                        .Assembly.GetName()
+                        .Version.ToString();
+                }
+                return _version;
+            }
+            
+        }
+    }
+}
