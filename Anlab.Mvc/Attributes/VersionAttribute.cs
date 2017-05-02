b1a81ce4d05f1e0c11462c6569087cc7241c97d4
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-05-02 09:32:06 -0700

    WIP Cache

---
+++ Anlab.Mvc/Attributes/VersionAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AnlabMvc.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class VersionAttribute : ActionFilterAttribute
+    {
+        public int MajorVersion { get; set; }
+        public string VersionKey { get; set; }
+
+        public VersionAttribute()
+        {
+            MajorVersion = 1;
+            VersionKey = "Version";
+        }
+
+        ///// <summary>
+        ///// Grabs the date time stamp and places the version in Cache if it does not exist
+        ///// and places the version in ViewData
+        ///// </summary>
+        ///// <param name="filterContext"></param>
+        //private void LoadAssemblyVersion(ActionExecutingContext filterContext)
+        //{
+        //    var version = filterContext.HttpContext.Cache[VersionKey] as string;
+
+        //    if (string.IsNullOrEmpty(version))
+        //    {
+        //        version = Assembly.GetExecutingAssembly().GetName().Version.ToString(); //Version from AppVeyor.
+
+        //        //Insert version into the cache until tomorrow (Today + 1 day)
+        //        filterContext.HttpContext.Cache.Insert(VersionKey, version, null, DateTime.Today.AddDays(1), Cache.NoSlidingExpiration);
+        //    }
+
+        //    filterContext.Controller.ViewData[VersionKey] = version;
+        //}
+
+        //public override void OnActionExecuting(ActionExecutingContext filterContext)
+        //{
+        //    LoadAssemblyVersion(filterContext);
+
+        //    base.OnActionExecuting(filterContext);
+        //}
+    }
+}
--- Anlab.Mvc/Startup.cs
+++ Anlab.Mvc/Startup.cs
@@ -65,6 +65,7 @@ namespace AnlabMvc
             services.AddTransient<IEmailSender, AuthMessageSender>();
             services.AddTransient<ISmsSender, AuthMessageSender>();
             services.AddTransient<IDirectorySearchService, DirectorySearchService>();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
