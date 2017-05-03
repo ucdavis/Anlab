625bceed132bdb3707f1f173f47e6e4ca5c9df75
Scott Kirkland <srkirkland@gmail.com>
2017-05-03 16:34:46 -0700

    more types plus swap of costs

--- Anlab.Mvc/Client/components/OrderForm.tsx
+++ Anlab.Mvc/Client/components/OrderForm.tsx
@@ -1,19 +1,25 @@
 ﻿import * as React from 'react';
 import { ITestItem, TestList } from './TestList';
+import { IPayment, PaymentSelection } from './PaymentSelection';
 
 declare var window: any;
 
 interface IOrderState {
+    payment: IPayment;
     testItems: Array<ITestItem>;
 }
 
-export default class OrderForm extends React.Component<any, IOrderState> {
-    state = { payment: {}, testItems: window.App.orderData.testItems };
+export default class OrderForm extends React.Component<undefined, IOrderState> {
+    state = { payment: { clientType: 'other' }, testItems: window.App.orderData.testItems };
 
+    onPaymentSelected = (payment: any) => {
+        this.setState({ ...this.state, payment });
+    }
     render() {
         return (
             <div>
-                <TestList items={this.state.testItems} />
+                <PaymentSelection payment={this.state.payment} onPaymentSelected={this.onPaymentSelected} />
+                <TestList items={this.state.testItems} payment={this.state.payment} />
             </div>
         );
     }
--- Anlab.Mvc/Client/components/TestList.tsx
+++ Anlab.Mvc/Client/components/TestList.tsx
@@ -1,4 +1,5 @@
 ﻿import * as React from 'react';
+import { IPayment } from './PaymentSelection';
 
 export interface ITestItem {
     id: number;
@@ -9,7 +10,8 @@ export interface ITestItem {
 }
 
 export interface ITestListProps {
-    items: Array<ITestItem>
+    items: Array<ITestItem>,
+    payment: IPayment,
 };
 
 export class TestList extends React.Component<ITestListProps, any> {
@@ -17,11 +19,12 @@ export class TestList extends React.Component<ITestListProps, any> {
 
     renderRows = () => {
         return this.props.items.map(item => {
+            const price = this.props.payment.clientType === 'uc' ? item.internalCost : item.externalCost;
             return (
                 <tr key={item.id}>
                     <td>{item.analysis}</td>
                     <td>{item.code}</td>
-                    <td>{item.internalCost}</td>
+                    <td>{price}</td>
                 </tr>
             );
         });
