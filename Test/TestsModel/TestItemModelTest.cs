511923eece0adc757dc06aedad07714329080981
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-05-03 08:20:00 -0700

    Model test for test item

---
+++ Test/TestsModel/TestItemModelTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Anlab.Core.Domain;
+using Test.Helpers;
+using Xunit;
+
+namespace Test.TestsModel
+{
+    [Trait("Category", "ModelTests")]
+    public class TestItemModelTests
+    {
+        [Fact]
+        public void TestFieldsHaveExpectedAttributes()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+            expectedFields.Add(new NameAndType("Analysis", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)512)]"
+            }));
+            expectedFields.Add(new NameAndType("Category", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)64)]"
+            }));
+            expectedFields.Add(new NameAndType("ChargeSet", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("Code", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)128)]"
+            }));
+            expectedFields.Add(new NameAndType("ExternalCost", "System.Decimal", new List<string>()));
+            expectedFields.Add(new NameAndType("FeeSchedule", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)7)]"
+            }));
+            expectedFields.Add(new NameAndType("Group", "System.String", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+                "[System.ComponentModel.DataAnnotations.StringLengthAttribute((Int32)8)]"
+            }));
+            expectedFields.Add(new NameAndType("GroupType", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.KeyAttribute()]"
+            }));
+            expectedFields.Add(new NameAndType("InternalCost", "System.Decimal", new List<string>()));
+            expectedFields.Add(new NameAndType("Multiplier", "System.Int32", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RangeAttribute((Int32)0, (Int32)2147483647)]"
+            }));
+            expectedFields.Add(new NameAndType("Multiplies", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("Notes", "System.String", new List<string>()));
+            expectedFields.Add(new NameAndType("Public", "System.Boolean", new List<string>()));
+            expectedFields.Add(new NameAndType("SetupCost", "System.Decimal", new List<string>()));
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(TestItem));
+        }
+
+    }
+}
