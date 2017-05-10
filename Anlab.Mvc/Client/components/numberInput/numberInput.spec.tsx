735fd23c34b6ce60fc2112c9e04c95871f1ac4bd
John Knoll <jpknoll@gmail.com>
2017-05-10 14:33:42 -0700

    spec

---
+++ Anlab.Mvc/Client/components/numberInput/numberInput.spec.tsx
@@ -0,0 +1,53 @@
+﻿import * as React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import { NumberInput } from './numberInput';
+
+describe('<NumberInput />', () => {
+    it('should render an input', () => {
+        const target = mount(<NumberInput />);
+        expect(target.find('input').length).toEqual(1);
+    });
+
+    it('should render a label', () => {
+        const target = mount(<NumberInput />);
+        expect(target.find('label').length).toEqual(1);
+    });
+
+    it('should load value into internalValue as string', () => {
+        const onChanged = jest.fn();
+        const target = mount(<NumberInput value={42} />);
+        const internal = target.instance();
+
+        expect(internal.state.internalValue).toBe('42');
+    });
+
+    it('should load value into internalValue on new props as string', () => {
+        const onChanged = jest.fn();
+        const target = mount(<NumberInput value={24} />);
+        const internal = target.instance();
+
+        target.setProps({value:42});
+
+        expect(internal.state.internalValue).toBe('42');
+    });
+
+    it('should call onChanged after blur event', () => {
+        const onChanged = jest.fn();
+        const target = mount(<NumberInput onChanged={onChanged} />);
+
+        target.find('input').simulate('blur');
+
+        expect(onChanged).toHaveBeenCalled();
+    });
+
+    it('should call onChanged with state.internalValue', () => {
+        const onChanged = jest.fn();
+        const target = mount(<NumberInput onChanged={onChanged} />);
+        const internal = target.instance()
+        internal.state.internalValue = 42
+
+        target.find('input').simulate('blur');
+        expect(onChanged).toHaveBeenLastCalledWith(42);
+    });
+});
