c3c4ce6be342495f02c9e7b8747bcedee31206f7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-05-10 10:15:17 -0700

    Radio button tool tip example

--- Anlab.Mvc/Client/components/SampleTypeSelection.tsx
+++ Anlab.Mvc/Client/components/SampleTypeSelection.tsx
@@ -1,4 +1,5 @@
 ﻿import * as React from 'react';
+import Tooltip from 'react-toolbox/lib/tooltip';
 import { RadioGroup, RadioButton } from 'react-toolbox/lib/radio';
 
 interface ISampleTypeProps {
@@ -6,16 +7,29 @@ interface ISampleTypeProps {
     onSampleSelected: Function;
 }
 
+const TooltipDiv = Tooltip(('div') as any);
+
+const style = {
+    position: 'absolute',
+    height: 20,
+    width: 200,
+    top: 0
+}
+
 export class SampleTypeSelection extends React.Component<ISampleTypeProps, any> {
     handleChange = (sampleType: string) => {
         this.props.onSampleSelected(sampleType);
     }
     render() {
-        return (            
+        return (
             <RadioGroup name='comic' value={this.props.sampleType} onChange={this.handleChange}>
-                <RadioButton label='Soil' value='Soil' />
-                <RadioButton label='Water' value='Water' />
-                <RadioButton label='Plant Material' value='Plant' />
+                <RadioButton label='Soil' value='Soil'>
+                    <TooltipDiv style={style} tooltip="A Soil Tooltip" tooltipDelay={500} tooltipPosition={'left'}/>
+                </RadioButton>
+                <RadioButton label='Water' value='Water'>
+                    <TooltipDiv style={style} tooltip="A Water Tooltip" tooltipDelay={500} tooltipPosition={'left'} />
+                </RadioButton>
+                <RadioButton label='Plant Material' value='Plant'/>
             </RadioGroup>
         );
     }
