8073e86821eb37dcf89eea17faeae43c18b41fcc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-05-18 11:34:32 -0700

    Validation on form for Project Id

--- Anlab.Mvc/Client/components/project.tsx
+++ Anlab.Mvc/Client/components/project.tsx
@@ -6,10 +6,43 @@ interface IProjectProps {
     handleChange: Function;
 }
 
-export class Project extends React.Component<IProjectProps, any> {
+interface IProjectInputState {
+    internalValue: string;
+    error: string;
+}
+
+export class Project extends React.Component<IProjectProps, IProjectInputState> {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            internalValue: this.props.project,
+            error: null
+        };
+    }
+
+    onChange = (v: string) => {
+        let error = null;
+        if (v.trim() === "") {
+            error = "The project id is required";
+        }
+        this.setState({ ...this.state, internalValue: v, error });
+        
+    }
+
+    onBlur = () => {
+        let error = null;
+        let internalValue = this.state.internalValue;
+        if (internalValue.trim() === "") {
+            error = "The project id is required";
+        }
+        this.setState({ ...this.state, error });
+        this.props.handleChange('project', internalValue);
+    }
     render() {
         return (
-            <Input type='text' required={true} maxLength={256} value={this.props.project} onChange={this.props.handleChange.bind(this, 'project')} label='Project Id' />
+            <Input type='text' onBlur={this.onBlur} error={this.state.error} required={true} maxLength={256} value={this.state.internalValue} onChange={this.onChange} label='Project Id' />
     );
 }
 }
