724a5053f9aa5e57ed552ac646f8f6f3d1a26132
Scott Kirkland <srkirkland@gmail.com>
2017-06-15 16:38:12 -0700

    move EF packages

--- Anlab.Core/Migrations/20170502215754_TestItems.cs
+++ Anlab.Core/Migrations/20170502215754_TestItems.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Anlab.Core.Migrations
 {
