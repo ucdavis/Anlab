6a0394c4df9bccc2c2dc1c30761c4d9197a606d0
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-19 14:47:00 -0700

    Starting AdditionalEmails.spec.tsx

---
+++ Anlab.Mvc/Client/components/specs/AdditionalEmails.spec.tsx
@@ -0,0 +1,18 @@
+﻿import * as React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { AdditionalEmails } from '../AdditionalEmails';
+
+describe('<AdditionalEmails />', () => {
+    it('should render an input', () => {
+        const target = mount(<AdditionalEmails addedEmails={[]} onDeleteEmail={null} onEmailAdded={null} />);
+        expect(target.find('input').length).toEqual(1);
+    });
+    it('should render an Button', () => {
+        const target = mount(<AdditionalEmails addedEmails={[]} onDeleteEmail={null} onEmailAdded={null} />);
+        expect(target.find('Button').length).toEqual(1);
+    });
+    it('should render an label', () => {
+        const target = mount(<AdditionalEmails addedEmails={[]} onDeleteEmail={null} onEmailAdded={null} />);
+        expect(target.find('label').length).toEqual(2);
+    });
+});
