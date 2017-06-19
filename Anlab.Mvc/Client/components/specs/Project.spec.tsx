3ed744436e9733538000a5f2d3a334b93367f4a3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-19 13:16:48 -0700

    Using numberInput specs as a template

--- Anlab.Mvc/Client/components/specs/Project.spec.tsx
+++ Anlab.Mvc/Client/components/specs/Project.spec.tsx
@@ -7,4 +7,31 @@ describe('<Project />', () => {
         const target = mount(<Project />);
         expect(target.find('input').length).toEqual(1);
     });
+    it('should load project into internalValue as string', () => {
+        const target = shallow(<Project project={'42'} />);
+        const internal = target.instance();
+
+        expect(internal.state.internalValue).toBe('42');
+    });
+
+    it('should not load project into internalValue on new props as string', () => {
+        const target = shallow(<Project project={'24'} />);
+        const internal = target.instance();
+
+        target.setProps({ project: '42' }); //Doesn't accept this
+
+        expect(internal.state.internalValue).toBe('24');
+    });
+
+    it('should call handleChange with state.internalValue on blur event', () => {
+        const handleChange = jasmine.createSpy('handleChange');
+        const target = shallow(<Project handleChange={handleChange} />);
+        const internal = target.instance();
+
+        internal.state.internalValue = 'test';
+        internal.onBlur();
+
+        expect(handleChange).toHaveBeenCalled();
+        expect(handleChange).toHaveBeenCalledWith('project','test');
+    });
 });
