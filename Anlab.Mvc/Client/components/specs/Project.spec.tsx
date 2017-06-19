b485a269c6ee75a6c91aa55cb23ea9d8ec6be38e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-19 11:12:42 -0700

    Begin Project.spec

---
+++ Anlab.Mvc/Client/components/specs/Project.spec.tsx
@@ -0,0 +1,10 @@
+﻿import * as React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { Project } from '../Project';
+
+describe('<Project />', () => {
+    it('should render an input', () => {
+        const target = mount(<Project />);
+        expect(target.find('input').length).toEqual(1);
+    });
+});
