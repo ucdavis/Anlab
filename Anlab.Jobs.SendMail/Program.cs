dae3c28625eb24778a1377f79ba040e160742f2d
Scott Kirkland <srkirkland@gmail.com>
2017-06-19 10:36:18 -0700

    core job for sending email in the queue

---
+++ Anlab.Jobs.SendMail/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anlab.Jobs.SendMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
