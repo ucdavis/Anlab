7bebef30ac4c266de7f2fed5934a445915d332e7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-20 07:41:31 -0700

    Test AdditionalEmails

--- Anlab.Mvc/Client/components/specs/AdditionalEmails.spec.tsx
+++ Anlab.Mvc/Client/components/specs/AdditionalEmails.spec.tsx
@@ -15,5 +15,28 @@ describe('<AdditionalEmails />', () => {
         const target = mount(<AdditionalEmails addedEmails={[]} onDeleteEmail={null} onEmailAdded={null} />);
         expect(target.find('label').length).toEqual(2);
     });
-
+    it('should render existing emails', () => {
+        const target = mount(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={null} />);
+        expect(target.find('Button').length).toEqual(4);
+    });
+    it('should render existing emails1', () => {
+        const target = mount(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={null} />);
+        const target2 = target.first('Button');
+        expect(target2.text()).toContain("test1@testy.com");
+    });
+    it('should render existing emails2', () => {
+        const target = mount(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={null} />);
+        const target2 = target.first('Button');
+        expect(target2.text()).toContain("test2@testy.com");
+    });
+    it('should render existing emails3', () => {
+        const target = mount(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={null} />);
+        const target2 = target.first('Button');
+        expect(target2.text()).toContain("test3@testy.com");
+    });
+    it('should render existing emails4', () => {
+        const target = mount(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={null} />);
+        const target2 = target.first('Button');
+        expect(target2.text()).not.toContain("test4@testy.com");
+    });
 });
