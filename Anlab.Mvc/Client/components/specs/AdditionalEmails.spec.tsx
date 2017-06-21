6590cdc3fd3a863c3fe656ad5c5eaa03fea90400
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-21 11:49:16 -0700

    AdditionalEmails.spec for changes to case and duplicate logic

--- Anlab.Mvc/Client/components/specs/AdditionalEmails.spec.tsx
+++ Anlab.Mvc/Client/components/specs/AdditionalEmails.spec.tsx
@@ -64,6 +64,19 @@ describe('<AdditionalEmails />', () => {
         expect(internal.state.email).toBe('');
     });
 
+    it('should call onEmailAdded with valid state.email onClick event and lower it', () => {
+        const onEmailAdded = jasmine.createSpy('onEmailAdded');
+        const target = shallow(<AdditionalEmails addedEmails={[]} onDeleteEmail={null} onEmailAdded={onEmailAdded} />);
+        const internal = target.instance();
+
+        internal.state.email = 'TEST@testy.COM';
+        internal.onClick();
+
+        expect(onEmailAdded).toHaveBeenCalled();
+        expect(onEmailAdded).toHaveBeenCalledWith('test@testy.com');
+        expect(internal.state.email).toBe('');
+    });
+
     it('should not call onEmailAdded with invalid state.email onClick event', () => {
         const onEmailAdded = jasmine.createSpy('onEmailAdded');
         spyOn(window, 'alert');
@@ -76,6 +89,34 @@ describe('<AdditionalEmails />', () => {
         expect(onEmailAdded).not.toHaveBeenCalled();
         expect(internal.state.email).toBe('test@invlid@invalid.com');
         expect(window.alert).toHaveBeenCalledWith('Invalid email');
+    });
+
+    it('should not call onEmailAdded with duplicate state.email onClick event', () => {
+        const onEmailAdded = jasmine.createSpy('onEmailAdded');
+        spyOn(window, 'alert');
+        const target = shallow(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={onEmailAdded} />);
+        const internal = target.instance();
+
+        internal.state.email = 'test2@testy.com';
+        internal.onClick();
+
+        expect(onEmailAdded).not.toHaveBeenCalled();
+        expect(internal.state.email).toBe('');
+        expect(window.alert).toHaveBeenCalledWith('Email already added');
+    });
+
+    it('should not call onEmailAdded with duplicate ignoring case state.email onClick event', () => {
+        const onEmailAdded = jasmine.createSpy('onEmailAdded');
+        spyOn(window, 'alert');
+        const target = shallow(<AdditionalEmails addedEmails={["test1@testy.com", "test2@testy.com", "test3@testy.com"]} onDeleteEmail={null} onEmailAdded={onEmailAdded} />);
+        const internal = target.instance();
+
+        internal.state.email = 'TEST2@testy.com';
+        internal.onClick();
+
+        expect(onEmailAdded).not.toHaveBeenCalled();
+        expect(internal.state.email).toBe('');
+        expect(window.alert).toHaveBeenCalledWith('Email already added');
     });
 
 
