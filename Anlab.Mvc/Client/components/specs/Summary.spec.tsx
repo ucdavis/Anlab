49d61d511313bff25929a9aba2e27235a46f04a3
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-26 14:51:38 -0700

    Internal cost calculation methods

--- Anlab.Mvc/Client/components/specs/Summary.spec.tsx
+++ Anlab.Mvc/Client/components/specs/Summary.spec.tsx
@@ -128,28 +128,228 @@ describe('<Summary />', () => {
                 expect(internal.foreignSoilCost()).toEqual(14);
             });
         });
+        describe('waterFilterCost internal function', () => {
+            it('should return zero when not a waterFilterCost parameter', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={false}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
 
-        it('should add stuff', () => {
-            
-            
+                const internal = target.instance();
+                expect(internal.waterFilterCost()).toEqual(0);
+            });
+            it('should return 11 when a waterFilterCost parameter and uc payment (Note: I expect this to change))', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={true}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={true}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
 
-            const payment = { clientType: 'uc', account: '' };
-            const target = shallow(<Summary adjustmentAmount={0}
-                isFromLab={false}
-                quantity={1}
-                payment={payment}
-                foreignSoil={false}
-                status="Test"
-                canSubmit={false}
-                filterWater={false}
-                grind={false}
-                hideError={true}
-                isCreate={true}
-                onSubmit={null}
-                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.waterFilterCost()).toEqual(11);
+            });
+            it('should return 17 when a waterFilterCost parameter and other payment (Note: I expect this to change))', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={true}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={true}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
 
-            const internal = target.instance();
-            expect(internal.totalCost()).toEqual(14.03);
+                const internal = target.instance();
+                expect(internal.waterFilterCost()).toEqual(17);
+            });
+        });
+        describe('totalCost internal function', () => {
+            it('should add up the cost with internal, quantity 1, no grind/filter/foreign', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={false}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(14.03);
+            });
+            it('should add up the cost with internal, quantity 3, no grind/filter/foreign', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={3}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={false}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(20.09); //(3 * 3.03) + 11
+            });
+            it('should add up the cost with external, quantity 1, no grind/filter/foreign', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={false}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(18.06); //(7.06 + 11 )
+            });
+            it('should add up the cost with external, quantity 3, no grind/filter/foreign', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={3}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={false}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(32.18); //(3 * 7.06) + 11
+            });
+            it('should add up the cost with internal, quantity 1, grind', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(20.03); //(3.03 + 6) + 11
+            });
+            it('should add up the cost with internal, quantity 3, grind', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={3}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(38.09); // 3 * (3.03 + 6) + 11
+            });
+            it('should add up the cost with external, quantity 1, grind', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={1}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(27.06);
+            });
+            it('should add up the cost with external, quantity 3, grind', () => {
+                const payment = { clientType: 'other', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={3}
+                                                payment={payment}
+                                                foreignSoil={false}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={false}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(59.18); 
+            });
+            it('should add up the cost with internal, quantity 3, grind, foriegn, filter', () => {
+                const payment = { clientType: 'uc', account: '' };
+                const target = shallow(<Summary adjustmentAmount={0}
+                                                isFromLab={false}
+                                                quantity={3}
+                                                payment={payment}
+                                                foreignSoil={true}
+                                                status="Test"
+                                                canSubmit={false}
+                                                filterWater={true}
+                                                grind={true}
+                                                hideError={true}
+                                                isCreate={true}
+                                                onSubmit={null}
+                                                testItems={testItems} />);
+                const internal = target.instance();
+                expect(internal.totalCost()).toEqual(98.09); // 3 * (3.03 + 6 + 9 + 11) + 11
+            });
         });
     });
 });
