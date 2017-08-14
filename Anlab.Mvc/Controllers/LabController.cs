76d62fbcbf95867712b26604a834f5992cfd382f
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-08-14 09:28:48 -0700

    Here too

--- Anlab.Mvc/Controllers/LabController.cs
+++ Anlab.Mvc/Controllers/LabController.cs
@@ -179,6 +179,9 @@ namespace AnlabMvc.Controllers
             var model = new OrderReviewModel();
             model.Order = order;
             model.OrderDetails = order.GetOrderDetails();
+            model.TestItems = _dbContext.TestItems
+                .Where(a => model.OrderDetails.SelectedTests.Select(s => s.Id).Contains(a.Id)).ToList();
+            model.HideLabDetails = false;
 
             return View(model);
         }
