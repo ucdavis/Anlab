7b5a26dccc1987f152faa41103de46fed35ac73d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-09-19 09:06:30 -0700

    Move to service

--- Anlab.Core/Services/SlothService.cs
+++ Anlab.Core/Services/SlothService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -8,6 +9,7 @@ using Anlab.Core.Data;
 using Anlab.Core.Domain;
 using Anlab.Core.Models;
 using Anlab.Jobs.MoneyMovement;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
@@ -17,6 +19,7 @@ namespace Anlab.Core.Services
     public interface ISlothService
     {
         Task<SlothResponseModel> MoveMoney(Order order);
+        Task<bool> ProcessCreditCards(IConfigurationRoot config);
 
         //TODO: Move the CreditCard one here.
     }
@@ -80,6 +83,54 @@ namespace Anlab.Core.Services
             }
 
             return null;
+        }
+
+        public async Task<bool> ProcessCreditCards(IConfigurationRoot config) //Have to pass here, can't get DI working for the job
+        {
+            var token = config.GetSection("Financial:SlothApiKey").Value;
+            var url = config.GetSection("Financial:SlothApiUrl").Value;
+            var orders = _dbContext.Orders.Include(i => i.ApprovedPayment).Where(a =>
+                a.ApprovedPayment != null && a.Paid && a.Status != OrderStatusCodes.Complete).ToList();
+            if (orders.Count == 0)
+            {
+                return false;
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"{url}Transactions/processor/");
+                client.DefaultRequestHeaders.Add("X-Auth-Token", token);
+
+                Console.WriteLine($"Processing {orders.Count} orders");
+                var updatedCount = 0;
+                foreach (var order in orders)
+                {
+                    var response = await client.GetAsync(order.ApprovedPayment.Transaction_Id);
+                    if (response.StatusCode == HttpStatusCode.NotFound)
+                    {
+                        continue;
+                    }
+                    if (response.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        continue;
+                    }
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var slothResponse = JsonConvert.DeserializeObject<SlothResponseModel>(content);
+
+                        updatedCount++;
+                        order.KfsTrackingNumber = slothResponse.KfsTrackingNumber;
+                        order.SlothTransactionId = slothResponse.Id.ToString();
+                        order.Status = OrderStatusCodes.Complete;
+                    }
+                }
+
+                await _dbContext.SaveChangesAsync();
+                Console.WriteLine($"Updated {updatedCount} orders");
+            }
+
+            return true;
         }
     }
 }
--- Anlab.Jobs.MoneyMovement/Program.cs
+++ Anlab.Jobs.MoneyMovement/Program.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Anlab.Core.Data;
-using Anlab.Core.Domain;
-using Anlab.Core.Models;
+﻿using Anlab.Core.Data;
 using Anlab.Core.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Anlab.Jobs.MoneyMovement
 {
@@ -50,11 +43,11 @@ namespace Anlab.Jobs.MoneyMovement
             services.AddTransient<ISlothService, SlothService>();
             Provider = services.BuildServiceProvider();
             
-            SlothService = Provider.GetService<ISlothService>();
-           
+            SlothService = Provider.GetService<ISlothService>();           
 
             Console.WriteLine("About to start Credit Cards");
-            var result = Task.Run(() => ProcessCreditCardOrders()).Result; //Wasn't able to debug this unless it returned a result...
+            //var result = Task.Run(() => ProcessCreditCardOrders()).Result; //Wasn't able to debug this unless it returned a result...
+            var result = Task.Run(() => SlothService.ProcessCreditCards(Configuration)).Result; //Wasn't able to debug this unless it returned a result...
             if (!result)
             {
                 Console.WriteLine("No CC Orders to process");
@@ -64,56 +57,5 @@ namespace Anlab.Jobs.MoneyMovement
            
             Console.WriteLine("Done");
         }
-
-        public static async Task<bool> ProcessCreditCardOrders()
-        {
-            var dbContext = Provider.GetService<ApplicationDbContext>();
-            var token = Configuration.GetSection("Financial:SlothApiKey").Value;
-            var url = Configuration.GetSection("Financial:SlothApiUrl").Value;
-            var orders = dbContext.Orders.Include(i => i.ApprovedPayment).Where(a =>
-                a.ApprovedPayment != null && a.Paid && a.Status != OrderStatusCodes.Complete).ToList();
-            if (orders.Count == 0)
-            {
-                return false;
-            }
-
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri($"{url}Transactions/processor/");
-                client.DefaultRequestHeaders.Add("X-Auth-Token", token);
-
-                Console.WriteLine($"Processing {orders.Count} orders");
-                var updatedCount = 0;
-                foreach (var order in orders)
-                {
-                    var response = await client.GetAsync(order.ApprovedPayment.Transaction_Id);
-                    if (response.StatusCode == HttpStatusCode.NotFound)
-                    {
-                        continue;
-                    }
-                    if (response.StatusCode == HttpStatusCode.NoContent)
-                    {
-                        continue;
-                    }
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var content = await response.Content.ReadAsStringAsync();
-                        var slothResponse = JsonConvert.DeserializeObject<SlothResponseModel>(content);
-
-                        updatedCount++;
-                        order.KfsTrackingNumber = slothResponse.KfsTrackingNumber;
-                        order.SlothTransactionId = slothResponse.Id.ToString();
-                        order.Status = OrderStatusCodes.Complete;
-                    }
-                }
-
-                await dbContext.SaveChangesAsync();
-                Console.WriteLine($"Updated {updatedCount} orders");
-            }
-
-            return true;
-        }
-
-
     }
 }
