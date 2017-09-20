8b2e05a42432d25f7fca93ea55b07ea53aa5faf3
Scott Kirkland <srkirkland@gmail.com>
2017-09-20 11:55:01 -0700

    load tsx using typescript loader

--- Anlab.Mvc/webpack.config.js
+++ Anlab.Mvc/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = (env) => {
         },
         module: {
             rules: [
-                { test: /\.(js|jsx|ts|tsx)$/, include: /Client/, exclude: /node_modules/, use: 'babel-loader' },
+                { test: /\.(js|jsx|ts)$/, include: /Client/, exclude: /node_modules/, use: 'babel-loader' },
                 { test: /\.tsx?$/, include: /Client/, exclude: /node_modules/, use: 'awesome-typescript-loader?silent=true' },
                 { test: /\.css$/, use: isDevBuild ? ['style-loader', cssLoader, 'postcss-loader'] : ExtractTextPlugin.extract({ use: cssLoader }) },
                 { test: /\.scss$/, use: isDevBuild ? ['style-loader', 'css-loader', 'sass-loader'] : ExtractTextPlugin.extract({ use: ['css-loader', 'sass-loader'] }) },
