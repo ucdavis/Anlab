5c41205ab793d64af33b74334664e7c77308421c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-09-21 09:56:47 -0700

    use regex

--- Anlab.Core/Models/AccountModel.cs
+++ Anlab.Core/Models/AccountModel.cs
@@ -9,37 +9,10 @@ namespace Anlab.Jobs.MoneyMovement
     {
         public AccountModel(string rawAccount)
         {
-            //var temp = rawAccount.Split('-');
-            //var tempSub = temp[1].Split('/');
-
-            //Chart = temp[0];
-            //if (tempSub.Length > 1)
-            //{
-            //    Account = tempSub[0];
-            //    SubAccount = tempSub[1];
-            //}
-            //else
-            //{
-            //    Account = temp[1];
-            //}
-            var temp = Regex.Split(rawAccount, @"(\w)-(\w{7})\/?(\w{5})?");
-            Chart = temp[1];
-            Account = temp[2];
-            SubAccount = temp[3];
-
-            //string pattern = @"(\w)-(\w{7})\/?(\w{5})?";
-            //Regex rgx = new Regex(pattern);
-            //foreach (Match match in rgx.Matches(rawAccount))
-            //{
-            //    var test1 = match.Value;
-            //    var zzz = 1;
-            //}
-            ////var temp2 = Regex.Matches(rawAccount, @"(\w)-(\w{7})\/?(\w{5})?");
-            ////foreach (var VARIABLE in temp2)
-            ////{
-            ////    var test = VARIABLE;
-            ////}
-            ////var aaa = 1;
+            var regx = Regex.Match(rawAccount, @"(\w)-(\w{7})\/?(\w{5})?");
+            Chart = regx.Groups[1].Value;
+            Account = regx.Groups[2].Value;
+            SubAccount = regx.Groups[3].Value;
         }
         public string Chart { get; set; }
         public string Account { get; set; }
