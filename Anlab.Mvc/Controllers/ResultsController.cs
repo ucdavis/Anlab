ed86a9bfe4ad13582d0ed2fca82821d630e52030
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-09-27 08:30:36 -0700

    Pay from results page

--- Anlab.Mvc/Controllers/ResultsController.cs
+++ Anlab.Mvc/Controllers/ResultsController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Anlab.Core.Data;
 using Anlab.Core.Models;
+using AnlabMvc.Models.Configuration;
 using Microsoft.EntityFrameworkCore;
 using AnlabMvc.Models.Order;
 using AnlabMvc.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
 
 namespace AnlabMvc.Controllers
 {
@@ -16,11 +18,17 @@ namespace AnlabMvc.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly IFileStorageService _fileStorageService;
+        private readonly CyberSourceSettings _cyberSourceSettings;
+        private readonly IDataSigningService _dataSigningService;
+        private readonly AppSettings _appSettings;
 
-        public ResultsController(ApplicationDbContext context, IFileStorageService fileStorageService)
+        public ResultsController(ApplicationDbContext context, IFileStorageService fileStorageService, IDataSigningService dataSigningService, IOptions<CyberSourceSettings> cyberSourceSettings, IOptions<AppSettings> appSettings)
         {
             _context = context;
             _fileStorageService = fileStorageService;
+            _dataSigningService = dataSigningService;
+            _appSettings = appSettings.Value;
+            _cyberSourceSettings = cyberSourceSettings.Value;
         }
 
         public async Task<IActionResult> Link(Guid id)
@@ -32,7 +40,7 @@ namespace AnlabMvc.Controllers
                 return NotFound();
             }
 
-            if (order.Status != OrderStatusCodes.Complete)
+            if (order.Status != OrderStatusCodes.Finalized && order.Status != OrderStatusCodes.Complete)
             {
                 return NotFound();
             }
@@ -40,6 +48,17 @@ namespace AnlabMvc.Controllers
             var model = new OrderReviewModel();
             model.Order = order;
             model.OrderDetails = order.GetOrderDetails();
+
+            ViewBag.ShowCCPay = false;
+            if (order.PaymentType == PaymentTypeCodes.CreditCard && !order.Paid)
+            {
+                ViewBag.ShowCCPay = true;
+                Dictionary<string, string> dictionary = SetDictionaryValues(order, order.Creator);
+
+                ViewBag.Signature = _dataSigningService.Sign(dictionary);
+                ViewBag.PaymentDictionary = dictionary;
+                ViewBag.CyberSourceUrl = _appSettings.CyberSourceUrl;
+            }
 
             return View(model);
         }
@@ -50,6 +69,34 @@ namespace AnlabMvc.Controllers
 
             var result = await _fileStorageService.GetSharedAccessSignature(order.ResultsFileIdentifier);
             return Redirect(result.AccessUrl);
+        }
+
+        private Dictionary<string, string> SetDictionaryValues(Anlab.Core.Domain.Order order, Anlab.Core.Domain.User user)
+        {
+            var dictionary = new Dictionary<string, string>();
+            dictionary.Add("transaction_type", "sale");
+            dictionary.Add("reference_number", order.Id.ToString());
+            dictionary.Add("amount", order.GetOrderDetails().GrandTotal.ToString("F2"));
+            dictionary.Add("currency", "USD");
+            dictionary.Add("access_key", _cyberSourceSettings.AccessKey);
+            dictionary.Add("profile_id", _cyberSourceSettings.ProfileId);
+            dictionary.Add("transaction_uuid", Guid.NewGuid().ToString());
+            dictionary.Add("signed_date_time", DateTime.UtcNow.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+            dictionary.Add("unsigned_field_names", string.Empty);
+            dictionary.Add("locale", "en");
+            dictionary.Add("bill_to_email", user.Email);
+
+
+            dictionary.Add("bill_to_forename", user.GetFirstName());
+            dictionary.Add("bill_to_surname", user.GetLastName());
+
+            dictionary.Add("bill_to_address_country", "US");
+            dictionary.Add("bill_to_address_state", "CA");
+
+
+            var fieldNames = string.Join(",", dictionary.Keys);
+            dictionary.Add("signed_field_names", "signed_field_names," + fieldNames);
+            return dictionary;
         }
     }
 }
