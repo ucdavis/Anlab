73cbc5f93a256c1793f218ab74806ba4e1ead6c1
Scott Kirkland <srkirkland@gmail.com>
2017-10-03 15:33:27 -0700

    New controller for displaying SOP

---
+++ Anlab.Mvc/Controllers/AnalysisController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Anlab.Core.Data;
+using AnlabMvc.Models.Analysis;
+using Markdig;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnlabMvc.Controllers
+{
+    public class AnalysisController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AnalysisController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        // ex: /analysis/soil/200
+        [Route("/analysis/{category}/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            // ignore category since the ID is all we need
+            var analysis = await _dbContext.AnalysisMethods.SingleOrDefaultAsync(x => x.Id == id);
+
+            var content = Markdown.ToHtml(analysis.Content);
+            
+            return View(new AnalysisMethodViewModel { AnalysisMethod = analysis, HtmlContent = content });
+        }
+    }
+}
---
+++ Anlab.Mvc/Models/Analysis/AnalysisMethodViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Anlab.Core.Domain;
+
+namespace AnlabMvc.Models.Analysis
+{
+    public class AnalysisMethodViewModel
+    {
+        public AnalysisMethod AnalysisMethod { get; set; }
+        public string HtmlContent { get; set; }
+    }
+}
