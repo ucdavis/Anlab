cdf6540abf7f90ff60cbcf6e414518b3d9a37212
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-05 10:43:01 -0700

    CRUD AnalysisMethods
    Create and List

---
+++ Anlab.Mvc/Controllers/AdminAnalysisController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Anlab.Core.Data;
+using Anlab.Core.Domain;
+using AnlabMvc.Models.Roles;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnlabMvc.Controllers
+{
+    [Authorize(Roles = RoleCodes.Admin)]
+    public class AdminAnalysisController : ApplicationController
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public AdminAnalysisController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: AdminAnalysis
+        public async Task<ActionResult> Index()
+        {
+            return View(await _dbContext.AnalysisMethods.ToListAsync());
+        }
+
+
+        // GET: AdminAnalysis/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminAnalysis/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(AnalysisMethod analysisMethod)
+        {
+            ModelState.Clear();
+            TryValidateModel(analysisMethod);
+
+            if (ModelState.IsValid)
+            {
+                if (await _dbContext.AnalysisMethods.AnyAsync(a => a.Id == analysisMethod.Id))
+                {
+                    ErrorMessage = "Id already in use";
+                    return View(analysisMethod);
+                }
+                _dbContext.Add(analysisMethod);
+                await _dbContext.SaveChangesAsync();
+                Message = "Test created";
+                return RedirectToAction("Index");
+            }
+            return View(analysisMethod);
+        }
+
+        // GET: AdminAnalysis/Edit/5
+            public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminAnalysis/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminAnalysis/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminAnalysis/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
