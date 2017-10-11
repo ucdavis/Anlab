9925efe66575c73b11c84d478fac7e0793355ced
John Knoll <jpknoll@gmail.com>
2017-10-11 16:33:49 -0700

    clean up
    use email regex

--- Anlab.Mvc/Client/components/AdditionalEmails.tsx
+++ Anlab.Mvc/Client/components/AdditionalEmails.tsx
@@ -1,12 +1,16 @@
-﻿import * as React from 'react';
-import Input from 'react-toolbox/lib/input';
-import { Button, IconButton } from 'react-toolbox/lib/button';
-import Chip from 'react-toolbox/lib/chip';
+﻿import * as React from "react";
+
+// ui
+import { Button, IconButton } from "react-toolbox/lib/button";
+import Chip from "react-toolbox/lib/chip";
+import Input from "react-toolbox/lib/input";
+
+import { validateEmail } from "../util/email";
 
 interface IAdditionalEmailsProps {
-    addedEmails: Array<string>;
-    onEmailAdded: Function;
-    onDeleteEmail: Function;
+    addedEmails: string[];
+    onEmailAdded: (email: string) => void;
+    onDeleteEmail: (email: string) => void;
     defaultEmail: string;
 }
 
@@ -17,104 +21,151 @@ interface IAdditionalEmailsState {
     errorText: string;
 }
 
-
-
 export class AdditionalEmails extends React.Component<IAdditionalEmailsProps, IAdditionalEmailsState> {
     constructor(props) {
         super(props);
         this.state = {
             email: "",
-            toggle: false,
+            errorText: "",
             hasError: false,
-            errorText: ""
+            toggle: false,
         };
     }
 
-    onEmailChanged = (e) => {
-        var newEmail = e.target.value;
-        this.setState({ ...this.state, email: newEmail });
-    }
-    _addEmailAddress = () => {
-        const emailtoAdd = this.state.email.toLowerCase();
-        
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if (re.test((emailtoAdd))) {
-            if (this.props.addedEmails.indexOf(emailtoAdd) === -1 && emailtoAdd !== this.props.defaultEmail) {
-                this.props.onEmailAdded(emailtoAdd);
-                this.setState({ ...this.state, email: "", hasError: false, errorText: "", toggle: false});
-            } else {
-                this.setState({ ...this.state, hasError: true, errorText: "Email already added" });
-            }
-        } else {
-            this.setState({ ...this.state, hasError: true, errorText: "Invalid email" });
-        }
-    }
-
-    onDelete = (email2Delete: any) => {
-        this.props.onDeleteEmail(email2Delete);
-    }
-
-    _renderEmails = () => {
-        if (this.props.addedEmails.length === 0) {
-            return null;
-        }
-
-        const emails = this.props.addedEmails.map(item => {
-            return (
-                <Chip key={item} deletable onDeleteClick={() => this.onDelete(item)} > { item }</Chip>
-            );
-        });
-
-
-        return emails;
-    }
-
-    handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            if (this.state.email === "")
-                this._toggleAddEmail();
-            else
-                this._addEmailAddress();
-        }
-    }
-
-    handleBlur = () => {
-        if (this.state.email != "") {
-            this._addEmailAddress();
-        }
-        else
-            this._toggleAddEmail();
-    }
-
-    _toggleAddEmail = () => {
-        this.setState(prevState => ({
-            email: "", hasError: false, errorText: "", toggle: !prevState.toggle
-        }));
-        
-    }
-
-    render() {
-        let inputStyle = {};
-        if (this.state.hasError) {
-            inputStyle = {
-                color: '#de3226'
-            }
-        }
+    public render() {
         return (
             <div className="form_wrap">
                 <label className="form_header">Who should be notified for this test?</label>
                 <div>
                     <Chip>{this.props.defaultEmail}</Chip>
                     {this._renderEmails()}
-                    {!this.state.toggle && <Chip onClick={this._toggleAddEmail} > <i className="emailAddIconStyle fa fa-plus" aria-hidden="true"></i></Chip>}
-                    {this.state.toggle &&
-                        <Chip>
-                        <input className="emailInput" value={this.state.email} style={inputStyle} onChange={this.onEmailChanged} onKeyPress={this.handleKeyPress} onBlur={this.handleBlur} autoFocus={true} />
-                        <i className="emailIconStyle fa fa-plus-circle" aria-hidden="true" onClick={this._addEmailAddress} ></i>
-                        </Chip>}
+                    {this._renderInput()}
                 </div>
-                <span className="emailError">{this.state.hasError && this.state.errorText}</span>
+                {this._renderError()}
             </div>
         );
+    }
+
+    private _renderEmails = () => {
+        if (this.props.addedEmails.length === 0) {
+            return null;
+        }
+
+        return this.props.addedEmails.map((item) => {
+            return (
+                <Chip key={item} deletable={true} onDeleteClick={this._onDelete.bind(this, item)}>
+                  {item}
+                </Chip>
+            );
+        });
+    }
+
+    private _renderInput = () => {
+      if (!this.state.toggle) {
+        return (
+          <Chip onClick={this._toggleAddEmail}>
+            <i className="emailAddIconStyle fa fa-plus" aria-hidden="true" />
+          </Chip>
+        );
+      }
+
+      let inputStyle = {};
+      if (this.state.hasError) {
+          inputStyle = {
+              color: "#de3226",
+          };
+      }
+
+      return (
+        <Chip>
+          <input
+            className="emailInput"
+            value={this.state.email}
+            style={inputStyle}
+            onChange={this._onEmailChanged}
+            onKeyPress={this._handleKeyPress}
+            onBlur={this._handleBlur}
+            autoFocus={true}
+          />
+          <i
+            className="emailIconStyle fa fa-plus-circle"
+            aria-hidden="true"
+            onClick={this._addEmailAddress}
+          />
+        </Chip>
+      );
+    }
+
+    private _renderError = () => {
+      if (!this.state.hasError || !this.state.errorText) {
+        return null;
+      }
+
+      return <span className="text-danger emailError">{this.state.errorText}</span>;
+    }
+
+    private _onEmailChanged = (e) => {
+        const newEmail = e.target.value;
+        this.setState({ ...this.state, email: newEmail });
+    }
+
+    private _addEmailAddress = () => {
+        const emailtoAdd = this.state.email.toLowerCase();
+
+        // check for valid email address
+        if (!validateEmail(emailtoAdd)) {
+          this.setState({
+            ...this.state,
+            errorText: "Invalid email",
+            hasError: true,
+          });
+          return;
+        }
+
+        // check for duplicate email
+        if (emailtoAdd === this.props.defaultEmail || this.props.addedEmails.indexOf(emailtoAdd) >= 0) {
+          this.setState({ ...this.state, hasError: true, errorText: "Email already added" });
+          return;
+        }
+
+        this.props.onEmailAdded(emailtoAdd);
+        this.setState({
+          ...this.state,
+          email: "",
+          errorText: "",
+          hasError: false,
+          toggle: false,
+        });
+    }
+
+    private _onDelete = (email2Delete: any) => {
+        this.props.onDeleteEmail(email2Delete);
+    }
+
+    private _handleKeyPress = (e) => {
+        if (e.key === "Enter") {
+            if (this.state.email === "") {
+                this._toggleAddEmail();
+            } else {
+                this._addEmailAddress();
+            }
+        }
+    }
+
+    private _handleBlur = () => {
+        if (this.state.email !== "") {
+            this._addEmailAddress();
+        } else {
+            this._toggleAddEmail();
+        }
+    }
+
+    private _toggleAddEmail = () => {
+        this.setState((prevState) => ({
+            email: "",
+            errorText: "",
+            hasError: false,
+            toggle: !prevState.toggle,
+        }));
     }
 }
