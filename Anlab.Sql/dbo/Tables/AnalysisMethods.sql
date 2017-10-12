47d5e42f7a019041866c5c7dfe6cabe537399e70
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-12 13:12:40 -0700

    Imported DB into project

---
+++ Anlab.Sql/dbo/Tables/AnalysisMethods.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[AnalysisMethods] (
+    [Id]       INT            NOT NULL,
+    [Category] NVARCHAR (MAX) NULL,
+    [Content]  NVARCHAR (MAX) NULL,
+    [Title]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_AnalysisMethods] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
