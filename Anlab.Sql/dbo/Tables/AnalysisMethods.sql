f3d6afa2d6d2e9a8371e256f5880ae1ef661a2ef
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-16 07:41:15 -0700

    Data Annotation Updates
    Put tests back

--- Anlab.Sql/dbo/Tables/AnalysisMethods.sql
+++ Anlab.Sql/dbo/Tables/AnalysisMethods.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[AnalysisMethods] (
     [Id]       INT            NOT NULL,
-    [Category] NVARCHAR (MAX) NULL,
-    [Content]  NVARCHAR (MAX) NULL,
-    [Title]    NVARCHAR (MAX) NULL,
+    [Category] NVARCHAR (256) NULL,
+    [Content]  NVARCHAR (MAX) NOT NULL,
+    [Title]    NVARCHAR (256) NOT NULL,
     CONSTRAINT [PK_AnalysisMethods] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
