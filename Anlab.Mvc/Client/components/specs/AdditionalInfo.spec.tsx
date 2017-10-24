cc36dd432d11814754a074f4fb9d1b85c3d326d6
John Knoll <jpknoll@gmail.com>
2017-10-24 10:54:59 -0700

    /things-and-stuff: Auto stash before merge of "jpk/things-and-stuff" and "origin/master"

--- Anlab.Mvc/Client/components/specs/AdditionalInfo.spec.tsx
+++ Anlab.Mvc/Client/components/specs/AdditionalInfo.spec.tsx
@@ -1,66 +1,48 @@
-﻿import * as React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import { AdditionalInfo } from '../AdditionalInfo';
+﻿import { mount, render, shallow } from "enzyme";
+import * as React from "react";
+import { AdditionalInfo, IAdditionalInfoProps } from "../AdditionalInfo";
 
-describe('<AdditionalInfo />', () => {
-    it('should render an input', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test" handleChange={handleChange} />);
-        expect(target.find('Input').length).toEqual(1);
+describe("<AdditionalInfo />", () => {
+
+    const defaultProps = {
+        handleChange: null,
+        name: "",
+        value: "",
+    } as IAdditionalInfoProps;
+
+    it("should render an input", () => {
+        const target = mount(<AdditionalInfo {...defaultProps} />);
+        expect(target).toContainReact(<input />);
     });
 
-    it('should have a maxLength of 2000', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('maxLength')).toEqual(2000);
+    it("should have a label", () => {
+        const target = mount(<AdditionalInfo {...defaultProps} />);
+        expect(target).toContainReact(<label>Additional Information</label>);
     });
 
-    it('should have a label', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('label')).toEqual('Additional Information');
+    it("should have a value of Test1", () => {
+        const handleChange = jasmine.createSpy("handleChange");
+        const target = mount(<AdditionalInfo {...defaultProps} />);
+        const input = target.find("Input");
+        expect(input.prop("value")).toEqual("Test1");
     });
 
-    it('should have be multiLine', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('multiline')).toEqual(true);
+    it("should have a value of Test2", () => {
+        const handleChange = jasmine.createSpy("handleChange");
+        const target = mount(<AdditionalInfo {...defaultProps} />);
+        const input = target.find("Input");
+        expect(input.prop("value")).toEqual("Test2");
     });
 
-    it('should have a type of text', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('type')).toEqual('text');
-    });
-
-    it('should have a value of Test1', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test1" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('value')).toEqual('Test1');
-    });
-
-    it('should have a value of Test2', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = mount(<AdditionalInfo additionalInfo="Test2" handleChange={handleChange} />);
-        const input = target.find('Input');
-        expect(input.prop('value')).toEqual('Test2');
-    });
-
-
-    it('should call handleChange with state.internalValue on blur event', () => {
-        const handleChange = jasmine.createSpy('handleChange');
-        const target = shallow(<AdditionalInfo additionalInfo="x" handleChange={handleChange}/>);
+    it("should call handleChange with state.internalValue on blur event", () => {
+        const handleChange = jasmine.createSpy("handleChange");
+        const target = shallow(<AdditionalInfo {...defaultProps} handleChange={handleChange}/>);
         const internal = target.instance();
 
         internal.state.internalValue = 'test';
         internal._onBlur();
 
         expect(handleChange).toHaveBeenCalled();
-        expect(handleChange).toHaveBeenCalledWith('additionalInfo', 'test');
+        expect(handleChange).toHaveBeenCalledWith("additionalInfo", "test");
     });
 });
