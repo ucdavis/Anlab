138dd5c6deca378759a09b6413c54f149b56a2b4
John Knoll <jpknoll@gmail.com>
2017-10-25 15:07:17 -0700

    pass in window values

--- Anlab.Mvc/Client/order.tsx
+++ Anlab.Mvc/Client/order.tsx
@@ -1,8 +1,29 @@
-﻿import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import OrderForm from './components/OrderForm';
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+import OrderForm, { IOrderFormProps } from "./components/OrderForm";
 
-ReactDOM.render(
-    <OrderForm />,
-    document.getElementById('react-app')
-);
+declare var window: any;
+
+// build test list form window
+const props = {
+    testItems: window.App.orderData.testItems,
+    defaultAccount: window.App.defaultAccount,
+    defaultEmail: window.App.defaultEmail,
+    defaultClientId: window.App.defaultClientId,
+} as IOrderFormProps;
+
+function renderApp() {
+    ReactDOM.render(
+        <OrderForm {...props} />,
+        document.getElementById("react-app"),
+    );
+}
+
+renderApp();
+
+// Allow Hot Module Replacement
+if (module.hot) {
+    module.hot.accept("./components/OrderForm", () => {
+        renderApp();
+    });
+}
