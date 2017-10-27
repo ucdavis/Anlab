3775ae350a5e11ca39f176c8bf19d760112f9408
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-27 12:23:32 -0700

    Fix account lookup.
    Adjust regex

--- Anlab.Core/Models/AccountModel.cs
+++ Anlab.Core/Models/AccountModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -9,11 +9,12 @@ namespace Anlab.Jobs.MoneyMovement
     {
         public AccountModel(string rawAccount)
         {
-            var regx = Regex.Match(rawAccount, @"(\w)-(\w{7})\/?(\w{5})?");
+            var regx = Regex.Match(rawAccount, @"^(\w)-(\w{7})\/?(\w{5})?$");
             Chart = regx.Groups[1].Value;
             Account = regx.Groups[2].Value;
             SubAccount = regx.Groups[3].Value;
         }
+
         public string Chart { get; set; }
         public string Account { get; set; }
         public string SubAccount { get; set; }
