6bfe3272f1e83d8cd74fed080db951de576c37dc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-10-30 10:09:19 -0700

    Put startup back

--- Anlab.Mvc/Startup.cs
+++ Anlab.Mvc/Startup.cs
@@ -69,7 +69,7 @@ namespace AnlabMvc
             if (_environment.IsDevelopment())
             {
                 services.AddDbContext<ApplicationDbContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                    options.UseSqlite("Data Source=anlab.db")
                 );
             }
             else
