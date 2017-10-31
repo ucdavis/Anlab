4189983778e98b2c559aad4827eccebf43623f5d
Laura Holstege <laholstege@ucdavis.edu>
2017-10-31 11:00:26 -0700

    change to "

--- Anlab.Mvc/Client/components/SampleTypeQuestions.tsx
+++ Anlab.Mvc/Client/components/SampleTypeQuestions.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import { SampleSoilQuestions } from './SampleSoilQuestions';
-import { SampleWaterQuestions } from './SampleWaterQuestions';
+import * as React from "react";
+import { SampleSoilQuestions } from "./SampleSoilQuestions";
+import { SampleWaterQuestions } from "./SampleWaterQuestions";
 import { SamplePlantQuestions } from "./SamplePlantQuestions";
 
 export interface ISampleTypeQuestions {
