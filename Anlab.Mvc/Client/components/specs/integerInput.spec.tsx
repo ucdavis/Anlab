c779bf3633a16dbd3ae510d31c2947e81f1198dd
Laura Holstege <laholstege@ucdavis.edu>
2017-11-07 16:30:12 -0800

    number input to integer only

--- Anlab.Mvc/Client/components/specs/integerInput.spec.tsx
+++ Anlab.Mvc/Client/components/specs/integerInput.spec.tsx
@@ -1,23 +1,23 @@
 import { mount, render, shallow } from "enzyme";
 import * as React from "react";
 
-import { NumberInput } from "../ui/numberInput/numberInput";
+import { IntegerInput } from "../ui/integerInput/integerInput";
 
-describe("<NumberInput />", () => {
+describe("<IntegerInput />", () => {
     it("should render an input", () => {
-        const target = mount(<NumberInput />);
+        const target = mount(<IntegerInput />);
         expect(target.find("input").length).toEqual(1);
     });
 
     it("should load value into internalValue as string", () => {
-        const target = shallow(<NumberInput value={42} />);
+        const target = shallow(<IntegerInput value={42} />);
         const internal = target.instance();
 
         expect(internal.state.internalValue).toBe("42");
     });
 
     it("should load value into internalValue on new props as string", () => {
-        const target = shallow(<NumberInput value={24} />);
+        const target = shallow(<IntegerInput value={24} />);
         const internal = target.instance();
 
         target.setProps({value: 42});
@@ -27,7 +27,7 @@ describe("<NumberInput />", () => {
 
     it("should call onChanged with state.internalValue on blur event", () => {
         const onChanged = jasmine.createSpy("onChanged");
-        const target = shallow(<NumberInput onChange={onChanged} />);
+        const target = shallow(<IntegerInput onChange={onChanged} />);
         const internal = target.instance();
 
         internal.state.internalValue = "42.5";
@@ -39,7 +39,7 @@ describe("<NumberInput />", () => {
 
     it("should call onChanged with truncated state.internalValue", () => {
         const onChanged = jasmine.createSpy("onChanged");
-        const target = shallow(<NumberInput onChange={onChanged} integer={true} />);
+        const target = shallow(<IntegerInput onChange={onChanged} />);
         const internal = target.instance();
 
         internal.state.internalValue = "42.5";
@@ -48,19 +48,8 @@ describe("<NumberInput />", () => {
         expect(onChanged).toHaveBeenCalledWith(42);
     });
 
-    it("should call onChanged without truncated state.internalValue when not integer", () => {
-        const onChanged = jasmine.createSpy("onChanged");
-        const target = shallow(<NumberInput onChange={onChanged} />);
-        const internal = target.instance();
-
-        internal.state.internalValue = "42.5";
-        internal.onBlur();
-
-        expect(onChanged).toHaveBeenCalledWith(42.5);
-    });
-
     it("should clear error on good value", () => {
-        const target = mount(<NumberInput min={10} max={20} />);
+        const target = mount(<IntegerInput min={10} max={20} />);
         const internal = target.instance();
 
         const inp = target.find('input');
@@ -71,7 +60,7 @@ describe("<NumberInput />", () => {
     });
 
     it("should set error on non-number value", () => {
-        const target = mount(<NumberInput />);
+        const target = mount(<IntegerInput />);
         const internal = target.instance();
 
         const inp = target.find('input');
@@ -83,7 +72,7 @@ describe("<NumberInput />", () => {
     });
 
     it("should set error on less than min value", () => {
-        const target = mount(<NumberInput min={10} />);
+        const target = mount(<IntegerInput min={10} />);
         const internal = target.instance();
 
         const inp = target.find('input');
@@ -95,7 +84,7 @@ describe("<NumberInput />", () => {
     });
 
     it("should set error on more than max value", () => {
-        const target = mount(<NumberInput max={10} />);
+        const target = mount(<IntegerInput max={10} />);
         const internal = target.instance();
 
         const inp = target.find('input');
--- Anlab.Mvc/Client/components/ui/integerInput/integerInput.tsx
+++ Anlab.Mvc/Client/components/ui/integerInput/integerInput.tsx
@@ -1,28 +1,22 @@
 import * as React from "react";
 import Input from "../input/input";
 
-interface INumberInputProps {
+interface IIntegerInputProps {
     name?: string;
     label?: string;
     value?: number;
     min?: number;
     max?: number;
-    integer?: boolean;
     required?: boolean;
-    onBlur?: () => void;
     onChange?: (value: number) => void;
     inputRef?: (element: HTMLInputElement) => void;
 }
 
-interface INumberInputState {
+interface IIntegerInputState {
     error: string;
 }
 
-export class NumberInput extends React.Component<INumberInputProps, INumberInputState> {
-
-    public static defaultProps: Partial<INumberInputProps> = {
-        integer: false,
-    };
+export class IntegerInput extends React.Component<IIntegerInputProps, IIntegerInputState> {
 
     constructor(props) {
         super(props);
@@ -32,9 +26,6 @@ export class NumberInput extends React.Component<INumberInputProps, INumberInput
         };
     }
 
-    //public componentWillReceiveProps(nextProps) {
-    //    this.setState({ internalValue: this.transformValue(nextProps.value) });
-    //}
 
     public render() {
         return (
@@ -45,7 +36,6 @@ export class NumberInput extends React.Component<INumberInputProps, INumberInput
                 error={this.state.error}
                 required={this.props.required}
                 onChange={this.onChange}
-                //onBlur={this.onBlur}
                 inputRef={this.props.inputRef}
             />
         );
@@ -72,7 +62,7 @@ export class NumberInput extends React.Component<INumberInputProps, INumberInput
             error = `Must be a number less than or equal to ${this.props.max}.`;
         }
 
-        this.setState({ error } as INumberInputState);
+        this.setState({ error } as IIntegerInputState);
     }
 
     private onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -81,25 +71,4 @@ export class NumberInput extends React.Component<INumberInputProps, INumberInput
         this.props.onChange(Number(value));
     }
 
-    //private onBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-    //    let value = Number(this.state.internalValue);
-
-    //    if (isNaN(value)) {
-    //        value = null;
-    //    }
-
-    //    // force integer
-    //    if (this.props.integer && !isNaN(value)) {
-    //      value = Math.floor(value);
-    //    }
-
-    //    // push possible changes, clear error
-    //    this.setState({
-    //      internalValue: this.transformValue(value),
-    //    });
-
-    //    this.validate(this.state.internalValue);
-
-    //    this.props.onChange(value);
-    //}
 }
