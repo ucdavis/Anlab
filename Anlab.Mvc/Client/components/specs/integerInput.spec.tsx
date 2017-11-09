04ee69851f4ef32cd14ff827ab6138d7d9644052
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-11-09 13:15:57 -0800

    WIP, specs

--- Anlab.Mvc/Client/components/specs/integerInput.spec.tsx
+++ Anlab.Mvc/Client/components/specs/integerInput.spec.tsx
@@ -9,52 +9,13 @@ describe("<IntegerInput />", () => {
         expect(target.find("input").length).toEqual(1);
     });
 
-    it("should load value into internalValue as string", () => {
-        const target = shallow(<IntegerInput value={42} />);
-        const internal = target.instance();
-
-        expect(internal.state.internalValue).toBe("42");
-    });
-
-    it("should load value into internalValue on new props as string", () => {
-        const target = shallow(<IntegerInput value={24} />);
-        const internal = target.instance();
-
-        target.setProps({value: 42});
-
-        expect(internal.state.internalValue).toBe("42");
-    });
-
-    it("should call onChanged with state.internalValue on blur event", () => {
-        const onChanged = jasmine.createSpy("onChanged");
-        const target = shallow(<IntegerInput onChange={onChanged} />);
-        const internal = target.instance();
-
-        internal.state.internalValue = "42.5";
-        internal.onBlur();
-
-        expect(onChanged).toHaveBeenCalled();
-        expect(onChanged).toHaveBeenCalledWith(42.5);
-    });
-
-    it("should call onChanged with truncated state.internalValue", () => {
-        const onChanged = jasmine.createSpy("onChanged");
-        const target = shallow(<IntegerInput onChange={onChanged} />);
-        const internal = target.instance();
-
-        internal.state.internalValue = "42.5";
-        internal.onBlur();
-
-        expect(onChanged).toHaveBeenCalledWith(42);
-    });
-
     it("should clear error on good value", () => {
         const target = mount(<IntegerInput min={10} max={20} />);
         const internal = target.instance();
 
         const inp = target.find('input');
 
-        inp.simulate('change', { target: { value: '15' } });  
+        inp.simulate('change', { target: { value: 15 } });  
 
         expect(internal.state.error).toBeNull();
     });
