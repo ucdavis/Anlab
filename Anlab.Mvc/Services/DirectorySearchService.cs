3da78f28a989a717e738e8da4d4237f2d19cd08b
Scott Kirkland <srkirkland@ucdavis.edu>
2018-01-08 11:18:20 -0800

    comments

--- Anlab.Mvc/Services/DirectorySearchService.cs
+++ Anlab.Mvc/Services/DirectorySearchService.cs
@@ -27,9 +27,11 @@ namespace AnlabMvc.Services
 
         public async Task<Person> GetByEmailAsync(string email)
         {
+            // find the contact via their email
             var ucdContactResult = await ietClient.Contacts.Search(ContactSearchField.email, email);
             var ucdContact = ucdContactResult.ResponseData.Results.First();
 
+            // now look up the whole person's record by ID including kerb
             var ucdKerbResult = await ietClient.Kerberos.Search(KerberosSearchField.iamId, ucdContact.IamId);
             var ucdKerbPerson = ucdKerbResult.ResponseData.Results.Single();
 
