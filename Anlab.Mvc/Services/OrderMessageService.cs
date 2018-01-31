019d7a26cfda37c4786f22e7a7437eb9c042e316
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-01-31 13:51:42 -0800

    additional emails here too

--- Anlab.Mvc/Services/OrderMessageService.cs
+++ Anlab.Mvc/Services/OrderMessageService.cs
@@ -103,12 +103,17 @@ namespace AnlabMvc.Services
             //TODO: change body of email, right now it is the same as OrderCreated
             var body = await _viewRenderService.RenderViewToStringAsync("Templates/_PaymentReceived", order);
 
+            var sendTo = order.Creator.Email;
+            if (!string.IsNullOrWhiteSpace(order.AdditionalEmails))
+            {
+                sendTo = $"{sendTo};{order.AdditionalEmails}";
+            }
 
             var message = new MailMessage
             {
                 Subject = subject,
                 Body = body,
-                SendTo = order.Creator.Email,
+                SendTo = sendTo,
                 Order = order,
                 User = order.Creator,
             };
