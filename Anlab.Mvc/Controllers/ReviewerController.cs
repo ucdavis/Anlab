2009528c40bfb7944994c8ecc22cba6a438b2c33
Laura Holstege <laholstege@ucdavis.edu>
2018-02-13 13:31:48 -0800

    setup

---
+++ Anlab.Mvc/Controllers/ReviewerController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Anlab.Core.Data;
+using Microsoft.EntityFrameworkCore;
+using Anlab.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using AnlabMvc.Models.Roles;
+
+namespace AnlabMvc.Controllers
+{
+    [Authorize(Roles = RoleCodes.Admin)]
+    public class ReviewerController : ApplicationController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _context.Orders.Where(a => a.Status == OrderStatusCodes.Finalized).ToArrayAsync();
+
+            return View(model);
+        }
+    }
+}
