d247e6be47c416407358357d9cd618a98f195431
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-09 15:26:49 -0700

    Check the account expiry date and closed value

--- Anlab.Mvc/Services/IFinancialService.cs
+++ Anlab.Mvc/Services/IFinancialService.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Anlab.Core.Extensions;
+using Newtonsoft.Json;
 
 namespace AnlabMvc.Services
 {
@@ -26,22 +28,37 @@ namespace AnlabMvc.Services
             string url;
             if (!String.IsNullOrWhiteSpace(accountModel.SubAccount))
             {
-                url = String.Format("{0}/subaccount/{1}/{2}/{3}/name", _appSettings.FinancialLookupUrl,
+                url = String.Format("{0}/subaccount/{1}/{2}/{3}", _appSettings.FinancialLookupUrl,
                     accountModel.Chart, accountModel.Account, accountModel.SubAccount);
             }
             else
             {
-                url = String.Format("{0}/account/{1}/{2}/name", _appSettings.FinancialLookupUrl, accountModel.Chart,
+                url = String.Format("{0}/account/{1}/{2}", _appSettings.FinancialLookupUrl, accountModel.Chart,
                     accountModel.Account);
             }
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(url);
                 response.EnsureSuccessStatusCode();
+
+
                 var contents = await response.Content.ReadAsStringAsync();
-                return contents;
+                var kfsDetails = JsonConvert.DeserializeObject<KfsLookup>(contents);
+                if (kfsDetails.Closed || kfsDetails.AccountExpirationDate != null && kfsDetails.AccountExpirationDate.Value <= DateTime.UtcNow.ToPacificTime())
+                {
+                    throw new Exception("Closed or expired");
+                }
+                return JsonConvert.SerializeObject(kfsDetails.AccountName);
             }
 
         }
+    }
+
+    public class KfsLookup
+    {
+        public string AccountName { get; set; }
+        public DateTime? AccountExpirationDate { get; set; }
+
+        public bool Closed { get; set; }
     }
 }
