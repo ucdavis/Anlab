ad330983bb8c9348ecd185764e3fec18590189db
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-18 13:18:47 -0700

    tests

--- Test/TestsController/LabControllerTests.cs
+++ Test/TestsController/LabControllerTests.cs
@@ -30,20 +30,20 @@ namespace Test.TestsController
         public LabControllerReflectionTests(ITestOutputHelper output)
         {
             this.output = output;
-            ControllerReflection = new ControllerReflection(this.output);
+            ControllerReflection = new ControllerReflection(this.output, typeof(LabController));
         }
-        protected readonly Type ControllerClass = typeof(LabController);
+        //protected readonly Type ControllerClass = typeof(LabController);
         
 
         [Fact]
         public void TestControllerClassAttributes()
         {
-            ControllerReflection.ControllerInherits(ControllerClass, "ApplicationController");
-            var authAttribute = ControllerReflection.ClassExpectedAttribute<AuthorizeAttribute>(ControllerClass, 3);
+            ControllerReflection.ControllerInherits("ApplicationController");
+            var authAttribute = ControllerReflection.ClassExpectedAttribute<AuthorizeAttribute>(3);
             authAttribute.ElementAt(0).Roles.ShouldBe($"{RoleCodes.Admin},{RoleCodes.LabUser}");
 
-            ControllerReflection.ClassExpectedAttribute<AutoValidateAntiforgeryTokenAttribute>(ControllerClass, 3);
-            ControllerReflection.ClassExpectedAttribute<ControllerAttribute>(ControllerClass, 3);
+            ControllerReflection.ClassExpectedAttribute<AutoValidateAntiforgeryTokenAttribute>(3);
+            ControllerReflection.ClassExpectedAttribute<ControllerAttribute>( 3);
         }
 
 
@@ -55,19 +55,34 @@ namespace Test.TestsController
 #else
             var countAdjustment = 0;
 #endif
-            output.WriteLine("Test 1");
-            ControllerReflection.MethodExpectedAttribute<HttpGetAttribute>(ControllerClass, "Orders", 1);
-            output.WriteLine("Test 2");
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>(ControllerClass, "Details", 1 + countAdjustment);
-            output.WriteLine("Test 3");
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>(ControllerClass, "AddRequestNumber", 1 + countAdjustment, false);
-            output.WriteLine("Test 4");
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>(ControllerClass, "AddRequestNumber", 2 + countAdjustment, true);
-            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>(ControllerClass, "AddRequestNumber", 2 + countAdjustment, true);
-            output.WriteLine("Test 5");
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>(ControllerClass, "Confirmation", 1 + countAdjustment, false);
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>(ControllerClass, "Confirmation", 2 + countAdjustment, true);
-            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>(ControllerClass, "Confirmation", 2 + countAdjustment, true);
+            ControllerReflection.MethodExpectedAttribute<HttpGetAttribute>("Orders", 1, "Orders-1", showListOfAttributes:false);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("Details", 1 + countAdjustment, "Details-1", showListOfAttributes: false);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("AddRequestNumber", 1 + countAdjustment, "AddRequestNumberGet-1", false, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("AddRequestNumber", 2 + countAdjustment, "AddRequestNumberPost-1", true, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>("AddRequestNumber", 2 + countAdjustment, "AddRequestNumberPost - 2", true, showListOfAttributes: false);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("Confirmation", 1 + countAdjustment, "ConfirmationGet-1", false, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("Confirmation", 2 + countAdjustment, "ConfirmationPost-1", true, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>("Confirmation", 2 + countAdjustment, "ConfirmationPost-2", true, showListOfAttributes: false);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("Finalize", 1 + countAdjustment, "FinalizeGet-1", false, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("Finalize", 2 + countAdjustment, "FinalizePost-1", true, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>("Finalize", 2 + countAdjustment, "FinalizePost-2", true, showListOfAttributes: false);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderGet-1", false, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<HttpGetAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderGet-2", false, showListOfAttributes: false);
+            var overrideOrderGetAuth = ControllerReflection.MethodExpectedAttribute<AuthorizeAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderGet-3", false, showListOfAttributes: false);
+            overrideOrderGetAuth.ElementAt(0).Roles.ShouldBe(RoleCodes.Admin);
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderPost-1", true, showListOfAttributes: false);
+            ControllerReflection.MethodExpectedAttribute<HttpPostAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderPost-2", true, showListOfAttributes: false);
+            var overrideOrderPostAuth = ControllerReflection.MethodExpectedAttribute<AuthorizeAttribute>("OverrideOrder", 3 + countAdjustment, "OverrideOrderPost-3", true, showListOfAttributes: false);
+            overrideOrderPostAuth.ElementAt(0).Roles.ShouldBe(RoleCodes.Admin);
+
+            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("JsonDetails", 2 + countAdjustment, "JsonDetails-1", false, showListOfAttributes: false);
+            var jsonDetailsAuth = ControllerReflection.MethodExpectedAttribute<AuthorizeAttribute>("JsonDetails", 2 + countAdjustment, "JsonDetails-2", false, showListOfAttributes: false);
+            jsonDetailsAuth.ElementAt(0).Roles.ShouldBe(RoleCodes.Admin);
         }
 
 
