b01c74ae3423a7568741fb1edfee71b88d8b27a7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-05-07 08:21:07 -0700

    Remove Emulate user (Use Impersonate controller instead)

--- Anlab.Mvc/Controllers/SystemController.cs
+++ Anlab.Mvc/Controllers/SystemController.cs
@@ -1,16 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Anlab.Core.Data;
-using Anlab.Core.Domain;
 using AnlabMvc.Models.Roles;
 using AnlabMvc.Services;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
-using Serilog;
+using System.Threading.Tasks;
 
 namespace AnlabMvc.Controllers
 {
@@ -18,33 +10,11 @@ namespace AnlabMvc.Controllers
     public class SystemController : ApplicationController
     {
         private readonly IDbInitializationService _dbInitializationService;
-        private readonly SignInManager<User> _signInManager;
-        private UserManager<User> _userManager;
 
-        public SystemController(IDbInitializationService dbInitializationService, SignInManager<User> signInManager, UserManager<User> userManager)
+        public SystemController(IDbInitializationService dbInitializationService)
         {
             _dbInitializationService = dbInitializationService;
-            _signInManager = signInManager;
-            _userManager = userManager;
         }
-
-        public async Task<IActionResult> Emulate(string id)
-        {
-            var saveUser = User.Identity.Name;
-            var user = await _userManager.FindByNameAsync(id);
-
-            if (user == null) return NotFound();
-
-            await _signInManager.SignOutAsync(); // sign out current user
-
-            await _signInManager.SignInAsync(user, false); // sign in new user
-
-            Message = $"Signed in as {id}";
-            Log.Information($"{saveUser} Emulation of {id}");
-
-            return RedirectToAction("Index", "Home");
-        }
-
         public async Task<IActionResult> ResetDb()
         {
 #if DEBUG
