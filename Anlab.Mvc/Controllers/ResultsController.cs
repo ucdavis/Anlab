1b06becb334f18765832ef3fb0c8f1d72eaa7f88
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-07-19 14:11:45 -0700

    close #627

--- Anlab.Mvc/Controllers/ResultsController.cs
+++ Anlab.Mvc/Controllers/ResultsController.cs
@@ -68,6 +68,8 @@ namespace AnlabMvc.Controllers
                 model.CyberSourceUrl = _appSettings.CyberSourceUrl;
             }
 
+            Message = "Use the buttons to download results, Pay, and other actions. Mouse over the buttons for more details.";
+
             return View(model);
         }
 
