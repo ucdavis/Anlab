4502a6a0d0a7091bf7318b12a3e6be84ee234c90
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-07-24 08:30:20 -0700

    test

--- Test/TestsController/HomeControllerTests.cs
+++ Test/TestsController/HomeControllerTests.cs
@@ -126,7 +126,7 @@ namespace Test.TestsController
             #endregion Act
 
             #region Assert
-            result.Count().ShouldBe(4);
+            result.Count().ShouldBe(5);
 
             #endregion Assert
         }
