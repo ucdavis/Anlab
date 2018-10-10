71373f05339ba8071a2ba1338ef9c53f2383f921
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-10-10 08:08:33 -0700

    Ugly form

--- Anlab.Mvc/Models/Reviewer/ReviewerTotalModel.cs
+++ Anlab.Mvc/Models/Reviewer/ReviewerTotalModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace AnlabMvc.Models.Reviewer
 {
     public class ReviewerTotalModel
     {
+        [Display(Name = "Finalized Start")]
         public DateTime? fStart { get; set; }
+
+        [Display(Name = "Finalized End")]
         public DateTime? fEnd { get; set; }
+
+        [Display(Name = "Created Start")]
         public DateTime? cStart { get; set; }
+
+        [Display(Name = "Created End")]
         public DateTime? cEnd { get; set; }
 
         public IList<Anlab.Core.Domain.Order> Orders { get; set; }
