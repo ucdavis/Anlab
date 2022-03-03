00190bcab9fff033d263d4c60e8c7410c4c289a3
Spruce Weber-Milne <swebermilne@ucdavis.edu>
2022-03-03 11:02:40 -0800

    Fix broken tests

--- Test/TestsController/SystemControllerTests.cs
+++ Test/TestsController/SystemControllerTests.cs
@@ -26,25 +26,7 @@ namespace Test.TestsController
             MockDbIntService = new Mock<IDbInitializationService>();
             Controller = new SystemController(MockDbIntService.Object);
         }
-#if DEBUG
-        [Fact]
-        public async Task TestResetDbCallsService()
-        {
-            // Arrange
 
-
-
-            // Act
-            var controllerResult = await Controller.ResetDb();
-
-            // Assert
-            var redirectResult = Assert.IsType<RedirectToActionResult>(controllerResult);
-            redirectResult.ActionName.ShouldBe("LogoutDirect");
-            redirectResult.ControllerName.ShouldBe("Account");
-
-            MockDbIntService.Verify(a => a.RecreateAndInitialize(), Times.Once);
-        }
-#else
         [Fact]
         public Task TestResetDbThrowsException()
         {
@@ -53,13 +35,15 @@ namespace Test.TestsController
             // Act
             var ex = Assert.ThrowsAsync<NotImplementedException>(async () => await Controller.ResetDb());
             // Assert
+#if DEBUG
+            ex.Result.Message.ShouldBe("Only enable this when working against a local database.");
+#else
             ex.Result.Message.ShouldBe("WHAT!!! Don't reset DB in Release!");
+#endif
             MockDbIntService.Verify(a => a.RecreateAndInitialize(), Times.Never);
 
             return Task.CompletedTask;
         }
-#endif
-
     }
 
     [Trait("Category", "Controller Reflection")]
@@ -91,20 +75,5 @@ namespace Test.TestsController
         {
             ControllerReflection.ControllerPublicMethods(1);
         }
-
-        [Fact]
-        public void TestControllerMethodAttributes()
-        {
-
-#if DEBUG
-            var countAdjustment = 1;
-#else
-            var countAdjustment = 0;
-#endif
-            //1
-            ControllerReflection.MethodExpectedAttribute<AsyncStateMachineAttribute>("ResetDb", 1 + countAdjustment, "ResetDb-1", showListOfAttributes: false);
-
-        }
-
     }
 }
