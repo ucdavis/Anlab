76f61541c412275b8e3685993108710b5d1b20f0
Scott Kirkland <srkirkland@gmail.com>
2022-04-21 11:38:46 -0700

    project spec looking good
    also firmed up transient iso fetch dep

--- Anlab.Mvc/ClientApp/src/components/specs/Project.test.tsx
+++ Anlab.Mvc/ClientApp/src/components/specs/Project.test.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 import { Project } from '../Project';
 
 describe('<Project />', () => {
-    it('should render without crashing', () => {
-        render(<Project project="1" handleChange={null} projectRef={null}/>);
-    });
-    it('should render an input', () => {
-        render(<Project project="1" handleChange={null} projectRef={null}/>);
-        
-        // expect(screen.getByText('Learn React')).toBeInTheDocument();
-        // expect(screen.getByLabelText('Project Title / Location')).toBeInTheDocument();
-        expect(screen.getByRole('textbox')).toBeInTheDocument();
+  it('should render without crashing', () => {
+    render(<Project project='1' handleChange={null} projectRef={null} />);
+  });
 
-    });
+  it('should render an input', () => {
+    render(<Project project='1' handleChange={null} projectRef={null} />);
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  it('should clear error on good value', async () => {
+    render(<Project project='1' handleChange={() => {}} projectRef={null} />);
+
+    const user = userEvent.setup()
+
+    // submit a blank value, which should cause an error
+    await user.clear(screen.getByRole('textbox'));
+    await user.tab();
+
+    expect(screen.getByText('The project Title is required')).toBeInTheDocument();
+
+    // put in a good value, which should clear the error
+    await user.type(screen.getByRole('textbox'), 'good value');
+    await user.tab();
+
+    expect(screen.queryByText('The project Title is required')).not.toBeInTheDocument();
+  });
 });
