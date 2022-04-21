1ef5a82d6d1d0afc7e0ca753d03c7d57bd8e9768
Scott Kirkland <srkirkland@gmail.com>
2022-04-21 14:18:09 -0700

    jestify addtl info tests

---
+++ Anlab.Mvc/ClientApp/src/components/specs/AdditionalInfo.test.tsx
@@ -0,0 +1,38 @@
+﻿import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { AdditionalInfo, IAdditionalInfoProps } from "../AdditionalInfo";
+
+describe("<AdditionalInfo />", () => {
+
+    const defaultProps = {
+        handleChange: null,
+        name: "",
+        value: "",
+    } as IAdditionalInfoProps;
+
+    it("should render an input", () => {
+        render(<AdditionalInfo {...defaultProps} />);
+
+        expect(screen.getByRole("textbox")).toBeInTheDocument();
+    });
+
+    it("should have a value of Test1", () => {
+        render(<AdditionalInfo {...defaultProps} value="Test1" />);
+
+        expect(screen.getByRole("textbox")).toHaveValue("Test1");
+    });
+
+    it("should call handleChange with on change event", async () => {
+        const handleChange = jest.fn() as (value: string) => void;
+
+        render(<AdditionalInfo {...defaultProps} handleChange={handleChange}/>);
+
+        const user = userEvent.setup();
+
+        await user.type(screen.getByRole("textbox"), "Test1");
+
+        expect(handleChange).toHaveBeenCalled();
+    });
+});
