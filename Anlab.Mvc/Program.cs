8e96a867b6b0bf4d9089022fb40fec5b7878885d
Scott Kirkland <srkirkland@gmail.com>
2022-07-28 09:24:43 -0700

    goodbye stackify

--- Anlab.Mvc/Program.cs
+++ Anlab.Mvc/Program.cs
@@ -8,7 +8,6 @@ using Serilog;
 using Serilog.Events;
 using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
-using StackifyLib;
 
 namespace AnlabMvc
 {
@@ -51,7 +50,6 @@ namespace AnlabMvc
               .Enrich.WithExceptionDetails()
               .Enrich.WithProperty("Application", loggingSection.GetValue<string>("AppName"))
               .Enrich.WithProperty("AppEnvironment", loggingSection.GetValue<string>("Environment"))
-              .WriteTo.Stackify()
               .WriteTo.Console();
       
 
