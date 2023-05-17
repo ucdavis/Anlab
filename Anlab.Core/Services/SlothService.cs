67e651cdb1de061ba455cf33442d254f4ea1a5f7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-05-17 10:32:24 -0700

    Use a helper method to get the URL

--- Anlab.Core/Services/SlothService.cs
+++ Anlab.Core/Services/SlothService.cs
@@ -46,17 +46,11 @@ namespace Anlab.Core.Services
         //TODO: Add validation?
         public async Task<SlothResponseModel> MoveMoney(Order order)
         {
-            if (_appSettings.SlothApiUrl.EndsWith("v1/", StringComparison.OrdinalIgnoreCase) || _appSettings.SlothApiUrl.EndsWith("v2/", StringComparison.OrdinalIgnoreCase))
-            {
-                Log.Error("Sloth SlothApiUrl should not end with version");
-                //Replace the end of the string
-                _appSettings.SlothApiUrl = _appSettings.SlothApiUrl.Substring(0, _appSettings.SlothApiUrl.Length - 3);
-            }
             var log = Log.ForContext("OrderId", order.Id);
             
             var orderDetails = order.GetOrderDetails();
             var token = _appSettings.SlothApiKey;
-            var url = _appSettings.SlothApiUrl;
+            var url = _appSettings.SafeSlothApiUrl;
             if (_aeSettings.UseCoA)
             {
                 url = $"{url}v2/"; //Config Change!!!
@@ -223,13 +217,7 @@ namespace Anlab.Core.Services
 
         public async Task MoneyHasMoved()
         {
-            if (_appSettings.SlothApiUrl.EndsWith("v1/", StringComparison.OrdinalIgnoreCase) || _appSettings.SlothApiUrl.EndsWith("v2/", StringComparison.OrdinalIgnoreCase))
-            {
-                Log.Error("Sloth SlothApiUrl should not end with version");
-                //Replace the end of the string
-                _appSettings.SlothApiUrl = _appSettings.SlothApiUrl.Substring(0, _appSettings.SlothApiUrl.Length - 3);
-            }
-            var url = _appSettings.SlothApiUrl;
+            var url = _appSettings.SafeSlothApiUrl;
             if (_aeSettings.UseCoA)
             {
                 url = $"{url}v2/"; //Config Change!!!
@@ -321,13 +309,7 @@ namespace Anlab.Core.Services
 
         public async Task ProcessCreditCards()
         {
-            if (_appSettings.SlothApiUrl.EndsWith("v1/", StringComparison.OrdinalIgnoreCase) || _appSettings.SlothApiUrl.EndsWith("v2/", StringComparison.OrdinalIgnoreCase))
-            {
-                Log.Error("Sloth SlothApiUrl should not end with version");
-                //Replace the end of the string
-                _appSettings.SlothApiUrl = _appSettings.SlothApiUrl.Substring(0, _appSettings.SlothApiUrl.Length - 3);
-            }
-            var url = _appSettings.SlothApiUrl;
+            var url = _appSettings.SafeSlothApiUrl;
             if (_aeSettings.UseCoA)
             {
                 url = $"{url}v2/"; //Config Change!!!
