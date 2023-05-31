d5675317e21bf3277b92a8db4aa341abb1e25393
Scott Kirkland <srkirkland@gmail.com>
2023-05-31 13:30:58 -0700

    pull default account

--- Anlab.Mvc/Services/DocumentSigningService.cs
+++ Anlab.Mvc/Services/DocumentSigningService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Anlab.Core.Data;
@@ -97,7 +98,8 @@ public class DocumentSigningService : IDocumentSigningService
 
         var user = GetAccountInfo(authToken);
 
-        var account = user.Accounts[0]; // TODO: what if there are multiple accounts?  need to select one somehow perhaps
+        // there should always be only one default account
+        var account = user.Accounts.First(a => a.IsDefault == "true");
 
         var acctId = account.AccountId;
         var basePath = account.BaseUri;
