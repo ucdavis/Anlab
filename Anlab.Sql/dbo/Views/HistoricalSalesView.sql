94871fccd56c13fcd373cd5e76338742ac3213b2
Jason Sylvestre <jsylvestre@ucdavis.edu>
2023-06-07 09:06:45 -0700

    Cast to types

--- Anlab.Sql/dbo/Views/HistoricalSalesView.sql
+++ Anlab.Sql/dbo/Views/HistoricalSalesView.sql
@@ -1,7 +1,7 @@
 CREATE VIEW dbo.HistoricalSalesView
 AS
-SELECT Id, DateFinalized, JSON_QUERY(JsonDetails, '$.SelectedTests') AS SelectedTests, JSON_VALUE(JsonDetails, '$.Payment.IsInternalClient') AS IsInternal, ISNULL(JSON_VALUE(JsonDetails, '$.RushMultiplier'), 1.0) AS Rush, JSON_VALUE(JsonDetails, '$.InternalProcessingFee') AS InternalProcessingFee, JSON_VALUE(JsonDetails, 
-           '$.ExternalProcessingFee') AS ExternalProcessingFee, JSON_VALUE(JsonDetails, '$.Quantity') AS Quantity
+SELECT Id, DateFinalized, JSON_QUERY(JsonDetails, '$.SelectedTests') AS SelectedTests, CAST(JSON_VALUE(JsonDetails, '$.Payment.IsInternalClient') AS bit) AS IsInternal, CAST(ISNULL(JSON_VALUE(JsonDetails, '$.RushMultiplier'), 1.0) AS decimal(10, 2)) AS Rush, CAST(JSON_VALUE(JsonDetails, '$.InternalProcessingFee') 
+           AS decimal(10, 2)) AS InternalProcessingFee, CAST(JSON_VALUE(JsonDetails, '$.ExternalProcessingFee') AS decimal(10, 2)) AS ExternalProcessingFee, CAST(JSON_VALUE(JsonDetails, '$.Quantity') AS int) AS Quantity
 FROM   dbo.Orders
 WHERE (DateFinalized IS NOT NULL)
 GO
