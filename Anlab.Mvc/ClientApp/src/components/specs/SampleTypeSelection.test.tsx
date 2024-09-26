e2c83b58c3af75de7691f523d9c6aaff9d4e0176
Jason Sylvestre <jsylvestre@ucdavis.edu>
2024-09-26 15:12:48 -0700

    here too

--- Anlab.Mvc/ClientApp/src/components/specs/SampleTypeSelection.test.tsx
+++ Anlab.Mvc/ClientApp/src/components/specs/SampleTypeSelection.test.tsx
@@ -60,7 +60,7 @@ describe("<SampleTypeSelection/>", () => {
       );
 
       const user = userEvent.setup();
-      await user.click(screen.getByText("Water"));
+      await user.click(screen.getByText("Water / Misc. Liquid"));
 
       expect(onSampleSelected).toHaveBeenCalled();
       expect(onSampleSelected).toHaveBeenCalledWith("Water");
