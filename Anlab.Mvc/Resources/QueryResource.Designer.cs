d86c00e61f194f696f53a0343eb1a3b736cc04d8
Jason Sylvestre <jsylvestre@ucdavis.edu>
2025-02-06 07:07:07 -0800

    Get if order is finished in labworks

--- Anlab.Mvc/Resources/QueryResource.Designer.cs
+++ Anlab.Mvc/Resources/QueryResource.Designer.cs
@@ -19,7 +19,7 @@ namespace AnlabMvc.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class QueryResource {
@@ -174,6 +174,21 @@ namespace AnlabMvc.Resources {
         internal static string AnlabTestsRunForOrder {
             get {
                 return ResourceManager.GetString("AnlabTestsRunForOrder", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to     SELECT TOP (10) 
+        ///      r.[RESULT]
+        ///FROM [LABWORKS].[dbo].[SAMPLE] s
+        ///INNER JOIN [LABWORKS].[dbo].[RESULT] r
+        ///    ON s.[SAMPNO] = r.[SAMPNO]
+        ///WHERE s.LOGBATCH = @requestNum
+        ///  AND r.ACODE = &apos;()&apos;.
+        /// </summary>
+        internal static string FinishedInLabworks {
+            get {
+                return ResourceManager.GetString("FinishedInLabworks", resourceCulture);
             }
         }
     }
